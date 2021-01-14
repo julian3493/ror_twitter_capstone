d53df3fc3c8f92ddd0875f98680089cf76924ad7
julian3493 <julianramosarevalo@gmail.com>
2021-01-14 14:26:58 -0500

    create login feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :sign_in?, :authenticated?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def sign_in?
+    current_user != nil
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
+  validates :fullname, presence: true, length: { in: 3..20 }
+  # rubocop:disable Style/MutableConstant
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # rubocop:enable Style/MutableConstant
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 end
