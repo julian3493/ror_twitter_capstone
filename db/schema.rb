d3815c3c575485237344f380f233a2cbe853895b
julian3493 <julianramosarevalo@gmail.com>
2021-01-19 11:20:49 -0500

    add opinion model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_195901) do
+ActiveRecord::Schema.define(version: 2021_01_19_155308) do
 
   create_table "followings", force: :cascade do |t|
     t.integer "user_id"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2021_01_18_195901) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["follower_id"], name: "index_followings_on_follower_id"
     t.index ["user_id"], name: "index_followings_on_user_id"
+  end
+
+  create_table "opinions", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_opinions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +41,5 @@ ActiveRecord::Schema.define(version: 2021_01_18_195901) do
 
   add_foreign_key "followings", "users"
   add_foreign_key "followings", "users", column: "follower_id"
+  add_foreign_key "opinions", "users"
 end
