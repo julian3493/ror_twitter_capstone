3ca471b017942a099729ab55a1d3113509a87447
julian3493 <julianramosarevalo@gmail.com>
2021-01-20 12:17:28 -0500

    change stylesheet file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
     session.delete(:username)
     session.delete(:user_id)
     session[:user_id] = nil
-    redirect_to users_path
+    redirect_to new_session_path
     flash.notice = 'Logged out!'
   end
 end
