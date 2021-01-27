e9e85783bccf7628db665222fcd50a077eda3dfa
julian3493 <julianramosarevalo@gmail.com>
2021-01-27 11:24:49 -0500

    add user info in right side of user page

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :followeds, class_name: 'Following', foreign_key: 'follower_id'
   has_many :followers, class_name: 'Following', foreign_key: 'user_id'
 
-  has_many :users, through: :followers
+  has_many :users, through: :followeds
 
   scope :not_followed, ->(query) { where.not(id: query.pluck(:user_id)) }
   scope :u_followers, ->(user) { where(id: user.followers.pluck(:follower_id))}
