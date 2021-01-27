6d7cdacaee8d114007898717a1d5c919fa4c793c
julian3493 <julianramosarevalo@gmail.com>
2021-01-27 15:58:34 -0500

    change follow validation

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -16,9 +16,9 @@ class FollowingsController < ApplicationController
     @following = Following.find_by(user_id: params[:id], follower_id: current_user.id)
     if @following
       @following.destroy
-      redirect_to root_path, notice: 'Friend has been removed'
+      redirect_to users_path, notice: 'Friend has been removed'
     else
-      redirect_to root_path, alert: 'You are not allowed to do this'
+      redirect_to users_path, alert: 'You are not allowed to do this'
     end
   end
 end
