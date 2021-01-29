92898f40a82b9d40102481686fe84eedfe11d14b
julian3493 <julianramosarevalo@gmail.com>
2021-01-29 12:17:11 -0500

    delete logic of the views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+  def follow_action(user)
+    if current_user.users.include?(user)
+      action = link_to('Unfollow', unfollow_user_path(user), method: :delete)
+    elsif current_user == user
+      action = ''
+    else
+      action = link_to('Follow', follow_user_path(user), method: :post)
+    end
+    return action
+  end
 end
