6f6fdbaf525224322a721767a8e7629477815585
julian3493 <julianramosarevalo@gmail.com>
2021-01-29 13:01:05 -0500

    fix rubocop issues

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,11 @@
 module UsersHelper
   def follow_action(user)
     if current_user.users.include?(user)
-      action = link_to('Unfollow', unfollow_user_path(user), method: :delete)
+      link_to('Unfollow', unfollow_user_path(user), method: :delete)
     elsif current_user == user
-      action = ''
+      ''
     else
-      action = link_to('Follow', follow_user_path(user), method: :post)
+      link_to('Follow', follow_user_path(user), method: :post)
     end
-    return action
   end
 end
